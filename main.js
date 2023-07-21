dc7658a76dcee91c164f2c71ea455a4afbe63ec7
Joseph Innocent <65666788+mrrfabulous@users.noreply.github.com>
2023-07-21 17:46:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 
-
 function toggleMode(x) {
     // x.classList.toggle("light_mode");
     var element = document.body;
@@ -19,7 +18,7 @@ function toggleMode(x) {
   
   window.addEventListener("mousewheel", function () {
           let home = document.getElementById('head')
-          if (window.scrollY > 450) {
+          if (window.scrollY > 100) {
               home.style.background = "var(--color-s-m-300)"
               home.style.transition = "0.5s"
   
@@ -29,6 +28,39 @@ function toggleMode(x) {
   
   })
   
+
+  let openBtn = document.getElementById('open-btn')
+  let closeBtn = document.getElementById('close-btn')
+  let nav = document.getElementById('nav')
+  let navItems = nav.children;
+  let items;
+    
+
+  let toggleActive = () => {
+    nav.classList.toggle('toggle')
+  };
+
+  openBtn.addEventListener('click', toggleActive)
+  closeBtn.addEventListener('click', toggleActive)
+
+
+  for (let i = 0; i <= navItems.length; i++) {
+      items = navItems[i];
+      items.addEventListener('click', toggleActive)
+    }
+
+  
+
+  
+
+
+ 
+
+
+
   
   
+
+
   
+ 
