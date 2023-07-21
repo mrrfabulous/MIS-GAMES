ae43e4043da2c5410c37e86b18f4644401d790cd
mrrfabulous <mrrfabulous8@gmail.com>
2023-07-21 22:24:43 +0100

    modified index, style and main

--- main.js
+++ main.js
@@ -1,66 +1,65 @@
 
 
 function toggleMode(x) {
-    // x.classList.toggle("light_mode");
-    var element = document.body;
-    element.classList.toggle("light-mode");
-    x.style.background= "none";
-    x.style.border= "none";
-    x.style.transition= "2s";
-    x.style.transform= "scale(0.9)";
-       
-    if (x.innerHTML === "dark_mode") {
-      x.innerHTML = "light_mode";
-    } else {
-      x.innerHTML = "dark_mode";
-    }
+  // x.classList.toggle("light_mode");
+  var element = document.body;
+  element.classList.toggle("light-mode");
+  x.style.background= "none";
+  x.style.border= "none";
+  x.style.transition= "2s";
+  x.style.transform= "scale(0.9)";
+     
+  if (x.innerHTML === "dark_mode") {
+    x.innerHTML = "light_mode";
+  } else {
+    x.innerHTML = "dark_mode";
   }
-  
-  window.addEventListener("mousewheel", function () {
-          let home = document.getElementById('head')
-          if (window.scrollY > 100) {
-              home.style.background = "var(--color-s-m-300)"
-              home.style.transition = "0.5s"
-  
-          } else {
-              home.style.background = "none"
-          }
-  
-  })
-  
+}
 
-  let openBtn = document.getElementById('open-btn')
-  let closeBtn = document.getElementById('close-btn')
-  let nav = document.getElementById('nav')
-  let navItems = nav.children;
-  let items;
-    
+window.addEventListener("mousewheel", function () {
+        let home = document.getElementById('head')
+        if (window.scrollY > 100) {
+            home.style.background = "var(--color-s-m-300)"
+            home.style.transition = "0.5s"
 
-  let toggleActive = () => {
-    nav.classList.toggle('toggle')
-  };
+        } else {
+            home.style.background = "none"
+        }
 
-  openBtn.addEventListener('click', toggleActive)
-  closeBtn.addEventListener('click', toggleActive)
+})
 
 
-  for (let i = 0; i <= navItems.length; i++) {
-      items = navItems[i];
-      items.addEventListener('click', toggleActive)
-    }
-
+let openBtn = document.getElementById('open-btn')
+let closeBtn = document.getElementById('close-btn')
+let nav = document.getElementById('nav')
+let navItems = nav.children;
+let items;
   
 
-  
+let toggleActive = () => {
+  nav.classList.toggle('toggle')
+};
+
+openBtn.addEventListener('click', toggleActive)
+closeBtn.addEventListener('click', toggleActive)
 
 
- 
+for (let i = 0; i <= navItems.length; i++) {
+    items = navItems[i];
+    items.addEventListener('click', toggleActive)
+  }
 
 
 
-  
-  
 
 
-  
- 
+
+
+
+
+
+
+
+
+
+
